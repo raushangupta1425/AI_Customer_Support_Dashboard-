771112e4e5fd8e3fe301c22fdb0f73d4de95cee0
Raushan Gupta <raushangupta1425@gmail.com>
2026-04-29 17:37:53 +0530

    connect backend deployed url

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,8 @@ export default defineConfig({
   server: {
     port: 5173,
     proxy: {
-      '/api': 'http://localhost:5000',
+      // '/api': 'http://localhost:5000',
+      '/api': 'https://ai-customer-support-dashboard-sw2c.onrender.com',
     },
   },
 });
